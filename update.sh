98ee38cff8cf476dda8f78b32a0190c6d2e32d2f
B Kleinen <kleinen@infrastructure.de>
2020-10-30 16:53:09 +0100

    update 2020-10-30---16-53-09

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget https://www.htw-berlin.de/coronavirus/faq-fuer-studierende/
+mv index.html studierende.html
+wget https://www.htw-berlin.de/coronavirus/faq-fuer-bewerber-innen/
+mv index.html bewerber_innen.html
+wget https://www.htw-berlin.de/coronavirus/faq-fuer-lehrende/
+mv index.html lehrende.html
+wget https://www.htw-berlin.de/coronavirus/faq-fuer-beschaeftigte/
+mv index.html beschaeftigte.html
+
+git add .
+git commit -m "update $(date +%Y-%m-%d---%H-%M-%S)"
