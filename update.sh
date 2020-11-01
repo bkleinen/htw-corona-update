f44fbfed6519afd8bdb36a20d83dc45e9656fddf
B Kleinen <kleinen@infrastructure.de>
2020-11-01 11:49:42 +0100

    insert cr after tags for better diff, rename files for easier loop

--- update.sh
+++ update.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-wget https://www.htw-berlin.de/coronavirus/faq-fuer-studierende/
-mv index.html studierende.html
-wget https://www.htw-berlin.de/coronavirus/faq-fuer-bewerber-innen/
-mv index.html bewerber_innen.html
-wget https://www.htw-berlin.de/coronavirus/faq-fuer-lehrende/
-mv index.html lehrende.html
-wget https://www.htw-berlin.de/coronavirus/faq-fuer-beschaeftigte/
-mv index.html beschaeftigte.html
-
+for faq in faq-fuer-studierende faq-fuer-bewerber-innen faq-fuer-lehrende faq-fuer-beschaeftigte; do
+  echo $faq
+  wget https://www.htw-berlin.de/coronavirus/$faq/
+  if [ $(uname -s) == "Darwin" ]; then
+    # needs gnu-sed on mac, brew install gnu-sed
+    echo "Mac"
+    gsed  's/>/>\n/g' index.html > $faq.html
+  else
+    echo "no mac"
+    sed  's/>/>\n/g' index.html > $faq.html
+  fi
+  rm index.html
+done
 git add .
 git commit -m "update $(date +%Y-%m-%d---%H-%M-%S)"
 git push origin main
